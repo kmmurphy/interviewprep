e66bcd87c52c851d3c8682461a6ae8f22be25aa5
Jeremy Stanley <github@azulus.com>
2013-06-05 18:17:36 -0700

    Added comments

--- solutions/algorithms/algorithm_bubble_sort.py
+++ solutions/algorithms/algorithm_bubble_sort.py
@@ -1,5 +1,6 @@
 import random
 
+# Basic bubble sort
 def bubbleSort(arr):
   newArr = arr[:]
   listLen = len(arr)
@@ -14,12 +15,14 @@ def bubbleSort(arr):
         swapped = True
   return newArr
 
+# Create an array to sort
 def createArrayToSort(len, min, max):
   arr = []
   for i in range(0, len):
     arr.append(random.randint(min, max))
   return arr
 
+# Compare two arrays and make sure they match
 def compareArrays(arr1, arr2):
   len1 = len(arr1)
   len2 = len(arr2)
@@ -32,10 +35,16 @@ def compareArrays(arr1, arr2):
       return False
   return True
 
+# Create the test array
 arr = createArrayToSort(1000, 0, 10000)
+
+# Clone and sort the test array using the built in functionality
 validationArr = sorted(arr)
+
+# Sort the array using the custom sort
 sortedArr = bubbleSort(arr)
 
+# Verify that the arrays match
 listsMatch = compareArrays(validationArr, sortedArr)
 if listsMatch:
   print "THE LISTS MATCH"
